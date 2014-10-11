baadb66e1b1da9742f88ce9c94c39f5b61b324b1
sagan <jacquesde@sagan.me>
2014-10-12 02:17:53 +0800

    Update udp_echo_client.py
    some minor change - -

--- udp_echo_client.py
+++ udp_echo_client.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
+
 import sys
 import time
 import socket
+import struct
 
 def usage():
     print('Usage: python udp_echo_client.py [-s <packetSize>] [-c <packetCount>] [-w <timeout>] [-l <localHost>] [-p <localPort>] <remoteHost> [<remotePort>]')
 
 remoteHost = None
-remotePort = None
+remotePort = 7
 packetSize = 56
 packetCount = 4
 timeout = 3000
@@ -58,10 +61,10 @@ print('Ping %s:%d with %d bytes for %d count:' % (remoteHost, remotePort, packet
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 
 s.bind((localHost, localPort))
-
 s.settimeout(timeout / 1000.0)
+s.setblocking(1)
 
-buf = bytearray([b % 0x100 for b in range(0, packetSize)])
+buf = bytearray([b % 0x100 for b in range(0, packetSize - 1)])
 
 minLatency = 0x7fffffff
 maxLatency = 0
@@ -72,11 +75,17 @@ receivedCount = 0
 try:
     for i in range(0, packetCount):
         beginTime = time.time()
-        s.sendto(buf, 0, (remoteHost, remotePort))
+        s.sendto(struct.pack("!B", i) + buf, 0, (remoteHost, remotePort))
         sentCount += 1
         try:
-            data, addr = s.recvfrom(65536)
-            elapsedTime = (time.time() - beginTime) * 1000
+            while True:
+                data, addr = s.recvfrom(65536)
+                feedbackid, = struct.unpack("!B", data[0:1])
+                if feedbackid == i:
+                    break
+                if ( time.time() - beginTime ) * 1000 > timeout:
+                    raise socket.timeout
+            elapsedTime = (time.time() - beginTime) * 1000.0
             if minLatency > elapsedTime:
                 minLatency = elapsedTime
             if maxLatency < elapsedTime:
@@ -84,7 +93,7 @@ try:
             allElapsedTime += elapsedTime
             receivedCount += 1
             print('Response from %s:%d: %d bytes, elapsedTime is %d' % (str(addr[0]), addr[1], len(data), int(elapsedTime)) + 'ms.')
-            time.sleep(1 - elapsedTime / 1000)
+            time.sleep(1 - elapsedTime / 1000.0)
         except socket.timeout:
             print('Request timed out.')
 except KeyboardInterrupt:
@@ -95,3 +104,4 @@ if sentCount != 0:
     print('All sent: %d count, received: %d count, lost: %d count, lost rate: %d%%.' % (sentCount, receivedCount, sentCount - receivedCount, (sentCount - receivedCount) * 100 / sentCount))
 if receivedCount != 0:
     print('min latency: ' + str(int(minLatency)) + 'ms, max latency: ' + str(int(maxLatency)) + 'ms, average lagenty: ' + str(int(allElapsedTime / receivedCount)) + 'ms.')
+    
